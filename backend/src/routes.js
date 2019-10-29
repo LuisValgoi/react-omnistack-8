c337cc94b202cdb230fba084f610eaa07a6868a8
Luis Valgoi <luis.valgoi@sap.com>
2019-10-29 00:58:10 -0300

    created store/create dev users via POST

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,7 @@
 const express = require("express");
-
 const routes = express.Router();
+const DevController = require("./controllers/DevController");
 
-routes.get("/", (req, res) => {
-  return res.send("hello world")
-});
-
-routes.post("/devs", (req, res) => {
-  console.log(req.body);
-  return res.json({ q: true});
-});
+routes.post("/devs", DevController.store);
 
 module.exports = routes; 
