e35d95c566c89996cc2227f67f569253ee9b6148
Luis Valgoi <luis.valgoi@sap.com>
2019-10-29 20:40:26 -0300

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const server = express();
 
 mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-q7f0j.mongodb.net/omnistack8?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
