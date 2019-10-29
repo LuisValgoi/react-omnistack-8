eaeb56ee5e5c4e6a798cfc33b93caf7e1d6cd737
Luis Valgoi <luis.valgoi@sap.com>
2019-10-29 15:05:01 -0300

    implemented dev like method

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,25 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    // validation
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev Not Exists"});
+    }
+
+    // update
+    loggedDev.likes.push(targetDev._id);
+
+    // persist
+    await loggedDev.save();
+
+    // return 
+    return res.json(loggedDev)
+  }
+};
