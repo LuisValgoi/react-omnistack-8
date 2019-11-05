030b9219b474b213ec82d673dfb205d6513cf37c
Luis Valgoi <luis.valgoi@sap.com>
2019-11-04 23:23:22 -0300

    added main page with its css

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import "./Main.css";
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import api from "../services/api";
 
 function Main({match}) {
-  return <h1>{match.params.id}</h1>
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async function loadUser() {
+      const response = await api.get("/devs", {
+        headers: { user:match.params.id }
+      });
+      
+      setUsers(response.data);
+    })();
+  }, [match.params.id]);
+
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev"></img>
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            <img className="avatar" src={user.avatar} alt=""></img>
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button">
+                <img src={like} alt="Like"></img>
+              </button>
+              <button type="button">
+                <img src={dislike} alt="Dislike"></img>
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
